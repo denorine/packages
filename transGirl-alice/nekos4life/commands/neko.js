65490179a80fabd9224d91b2d3983c707fd0adfa
Alice Addison <30693865+transGirl-alice@users.noreply.github.com>
2021-03-29 01:14:34 -0400

    Update neko.js

--- transGirl-alice/nekos4life/commands/neko.js
+++ transGirl-alice/nekos4life/commands/neko.js
@@ -3,7 +3,37 @@ module.exports = {
     description: ':smirk_face:',
     execute(message, args, Discord, client ,version) {
 		const fetch = require("node-fetch");
-        
+let sfw;
+switch(args[0]) {
+case 'smug':
+case 'baka':
+case 'tickle':
+case 'slap':
+case 'poke':
+case 'pat':
+case 'neko':
+case 'nekoGif':
+case 'meow':
+case 'lizard':
+case 'kiss':
+case 'hug':
+case 'foxGirl':
+case 'feed':
+case 'cuddle':
+case 'kemonomimi':
+case 'holo':
+case 'woof':
+case 'wallpaper':
+case 'goose':
+case 'gecg':
+case 'avatar':
+case 'waifu':
+    sfw=true;
+}
+if (!sfw) {
+    message.reply("Oops, either the command isn't sfw or it isn't a real command")
+    return;
+}
         //fetch
         fetch(`https://nekos.life/api/v2/img/${args[0]}`)
             .then(response => response.json()) //json
