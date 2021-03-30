259bc8f9f1c663e1cfb2562dbad7d0df4389f8c7
Alice Addison <30693865+transGirl-alice@users.noreply.github.com>
2021-03-30 00:00:37 -0400

    Create lockdown.js

---
+++ transGirl-alice/server-lockdown/src/commands/lockdown.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'lockdown',
+    description: 'USE INCASE OF EMERGENCIES',
+    execute(message, args, Discord, client ,version) {
+        const fs = require("fs")
+        if (args[0] == "unlock") {
+            let channelsPerms = new Map (JSON.parse(fs.readFileSync("lockdown"))) //load permissions from file system
+            message.guild.channels.cache.array().forEach(channel=>{
+                channel.overwritePermissions(channelsPerms.get(channel.id)) // set permissions to previous settings
+            })
+        } else {
+            let t = new Map() // init map
+            message.guild.channels.cache.array().forEach(channel=>{ // for each channel 
+                t.set(channel.id, channel.permissionOverwrites) // store permissions in map
+                channel.updateOverwrite(message.mentions.roles.first(), {SEND_MESSAGES: false}) // set the send message permission to false
+            })
+            fs.writeFileSync("lockdown", JSON.stringify([...t])); // store permissions on filesystem
+        }
+    },
+}
