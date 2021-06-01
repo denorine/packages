cd0fe298289a038aa497d95dae89b1ef2a47a5d6
Esin Koseoglu <85190747+AFellowEsin@users.noreply.github.com>
2021-06-01 21:47:58 +0300

    Create trollge.js

---
+++ esin/shitpost/src/commands/trollge.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'trollge',
+	description: 'Do you like how I walk?',
+	execute(message) {
+		message.channel.send('https://tenor.com/view/troll-pilled-gif-19289988');
+	},
+};
