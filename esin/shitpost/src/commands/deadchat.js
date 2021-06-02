d09a9f92c4dc130bfcc7c992040f7513d038776c
Esin <85190747+AFellowEsin@users.noreply.github.com>
2021-06-02 09:47:03 +0300

    Create deadchat.js

---
+++ esin/shitpost/src/commands/deadchat.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'deadchat',
+	description: 'Use this command when the chat is dead',
+	execute(message) {
+		message.channel.send('Mods, can we get a dead chat ping here?');
+    message.channel.send('https://tenor.com/view/dead-chat-gif-21398098');
+    message.channel.send('EVERYBODY WAKE THE FUCK UP!');
+	},
+};
