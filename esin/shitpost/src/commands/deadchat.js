f52b5626465adbb62dff8e2c8a557e5bf7d4677d
Esin <85190747+AFellowEsin@users.noreply.github.com>
2021-06-07 20:27:21 +0300

    Update deadchat.js

--- esin/shitpost/src/commands/deadchat.js
+++ esin/shitpost/src/commands/deadchat.js
@@ -4,18 +4,18 @@ module.exports = {
 	execute(message) {
 		let montecarlo = Math.floor(Math.random()*3)
 		message.channel.send('Mods, can we get a dead chat ping here?');
-		if (montecarlo == 0) {
-    			message.channel.send('https://tenor.com/view/dead-chat-gif-21398098');
-		}
-		if (montecarlo == 1) {
-    			message.channel.send('https://tenor.com/view/dead-chat-gif-21375725');
-		}
-		if (montecarlo == 2) {
-    			message.channel.send('https://tenor.com/view/dead-chat-xd-dead-chat-plague-doctor-scp049-gif-20796096');
-		}
-		if (montecarlo == 3) {
-    			message.channel.send('https://tenor.com/view/nikocado-avocado-nikocado-mental-breakdown-mukbang-rage-mukbang-gif-21266615');
-		}
+			case 0:
+				message.channel.send('https://tenor.com/view/dead-chat-gif-21398098');
+				break;
+			case 1:
+				message.channel.send('https://tenor.com/view/dead-chat-gif-21375725');
+				break;
+			case 2:
+				message.channel.send('https://tenor.com/view/dead-chat-xd-dead-chat-plague-doctor-scp049-gif-20796096');
+				break;
+			case 3:
+				message.channel.send('https://tenor.com/view/nikocado-avocado-nikocado-mental-breakdown-mukbang-rage-mukbang-gif-21266615');
+				break;
     		message.channel.send('EVERYBODY WAKE THE FUCK UP!');
 	},
 };
