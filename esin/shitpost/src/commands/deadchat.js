24197b9973da91f37f78af9c6bc0f1872791acd7
Esin <85190747+AFellowEsin@users.noreply.github.com>
2021-06-07 11:58:18 +0300

    Update deadchat.js

--- esin/shitpost/src/commands/deadchat.js
+++ esin/shitpost/src/commands/deadchat.js
@@ -1,9 +1,21 @@
+var montecarlo = mathRandomInt(1,4)
 module.exports = {
 	name: 'deadchat',
 	description: 'Use this command when the chat is dead',
 	execute(message) {
 		message.channel.send('Mods, can we get a dead chat ping here?');
+		if (montecarlo == 1) {
     			message.channel.send('https://tenor.com/view/dead-chat-gif-21398098');
-    				message.channel.send('EVERYBODY WAKE THE FUCK UP!');
+		}
+		if (montecarlo == 2) {
+    			message.channel.send('https://tenor.com/view/dead-chat-gif-21375725');
+		}
+		if (montecarlo == 3) {
+    			message.channel.send('https://tenor.com/view/dead-chat-xd-dead-chat-plague-doctor-scp049-gif-20796096');
+		}
+		if (montecarlo == 4) {
+    			message.channel.send('https://tenor.com/view/nikocado-avocado-nikocado-mental-breakdown-mukbang-rage-mukbang-gif-21266615');
+		}
+    		message.channel.send('EVERYBODY WAKE THE FUCK UP!');
 	},
 };
