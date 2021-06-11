bd3b4536d053d5c801dddafea7c109f077c686a1
Esin <85190747+AFellowEsin@users.noreply.github.com>
2021-06-11 19:36:05 +0300

    Dw

---
+++ esin/shitpost/src/commands/saythenword.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'saythenword',
+	description: 'what the actual fuck-',
+	execute(message) {
+		message.channel.send('No');
+    message.channel.send('Because I am this:');
+   	message.channel.send('https://tenor.com/view/gigachad-chad-gif-20773266');
+	},
+};
