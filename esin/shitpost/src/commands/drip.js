64c8986c66bc2d4bc0c57b0de6c5e941f0578175
Esin <85190747+AFellowEsin@users.noreply.github.com>
2021-06-19 19:57:34 +0300

    Update drip.js

--- esin/shitpost/src/commands/drip.js
+++ esin/shitpost/src/commands/drip.js
@@ -2,6 +2,8 @@ module.exports = {
 	name: 'drip',
 	description: 'amogus',
 	execute(message) {
+		message.channel.send('https://tenor.com/view/drip-too-hard-among-drip-drip-poggers-among-us-gif-19670490');
 		message.channel.send('https://www.youtube.com/watch?v=bI25T1s1mAY');
+		message.channel.send('amogus');
 	},
 };
