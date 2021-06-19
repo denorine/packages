a9e554cbfdf6d5544739bf07da34020d7ee255b7
Esin <85190747+AFellowEsin@users.noreply.github.com>
2021-06-19 20:43:11 +0300

    Forgot something

--- esin/shitpost/src/commands/gachaheat.js
+++ esin/shitpost/src/commands/gachaheat.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'Hot Gacha heat uwu',
 	execute(message) {
 		message.channel.send('That\'s a bit sus :flushed:');
-        message.channel.send('\(TW: Loud music\) https://www.youtube.com/watch?v=XjNRm4N05uQ');
+        message.channel.send('\(TW: Loud music\) ||https://www.youtube.com/watch?v=XjNRm4N05uQ||');
    	    message.channel.send('I think you should get some help');
 	},
 };
