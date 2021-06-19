5c2d53ba3ae28a2cc831ceb5c2ee9b3c747cf805
Esin <85190747+AFellowEsin@users.noreply.github.com>
2021-06-19 20:39:17 +0300

    among us in real life

---
+++ esin/shitpost/src/commands/gachaheat.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'gachaheat',
+	description: 'Hot Gacha heat uwu',
+	execute(message) {
+		message.channel.send('That\'s a bit sus :flushed:');
+        message.channel.send('\(TW: Loud music\) https://www.youtube.com/watch?v=XjNRm4N05uQ');
+   	    message.channel.send('I think you should get some help');
+	},
+};
