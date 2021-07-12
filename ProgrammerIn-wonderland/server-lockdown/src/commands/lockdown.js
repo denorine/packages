698d90b155a5efecab3c53c2344a089068ffb439
femboy-neal <3838shah@gmail.com>
2021-07-12 00:11:55 -0400

    git new name 2

---
+++ ProgrammerIn-wonderland/server-lockdown/src/commands/lockdown.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: 'lockdown',
+    description: 'USE INCASE OF EMERGENCIES',
+    execute(message, args, Discord, client ,version) {
+        if(!message.member.roles.cache.find(r => r.name === 'gex')) return message.channel.send('You do not have gex.');
+        const fs = require("fs")
+        if (args[0] == "unlock") {
+            let channelsPerms = new Map (JSON.parse(fs.readFileSync("lockdown"))) //load permissions from file system
+            message.guild.channels.cache.array().forEach(channel=>{
+                channel.overwritePermissions(channelsPerms.get(channel.id)) // set permissions to previous settings
+            })
+            message.reply("Unlocked server")
+            fs.unlinkSync("lockdown");
+        } else {
+            if(fs.existsSync("lockdown")) return;
+            let t = new Map() // init map
+            message.guild.channels.cache.array().forEach(channel=>{ // for each channel 
+                t.set(channel.id, channel.permissionOverwrites) // store permissions in map
+                channel.updateOverwrite(message.mentions.roles.first(), {SEND_MESSAGES: false}) // set the send message permission to false
+            })
+            message.reply("Server has been lock down")
+            fs.writeFileSync("lockdown", JSON.stringify([...t])); // store permissions on filesystem
+        }
+    },
+}
